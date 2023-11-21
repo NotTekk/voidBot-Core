9f663df4b31f420a7392ed00f8eefee311f45655
Thiago Benkendorf <me@tekren.net>
2023-11-21 02:19:33 +0100

    Base Commit (Project Structure).

---
+++ src/main/java/dev/nottek/voidBot/core/Main.java
@@ -0,0 +1,9 @@
+package dev.nottek.voidBot.core;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
---
+++ src/main/java/dev/nottek/voidBot/core/handlers/ConfigHandler.java
@@ -0,0 +1,38 @@
+package dev.nottek.voidBot.core.handlers;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.List;
+
+public class ConfigHandler {
+
+    private final String[] dotenvFilePaths;
+    private static Dotenv[] dotenvFiles;
+
+    public ConfigHandler(final String[] dotenvFilePaths) {
+        this.dotenvFilePaths = dotenvFilePaths;
+    }
+
+    public void load() {
+        List<Dotenv> res = null;
+        for (String dotenvFilePath : dotenvFilePaths) {
+            res.add(Dotenv.configure()
+                    .directory(dotenvFilePath)
+                    .load());
+        }
+
+        if (null != res) dotenvFiles = res.toArray(new Dotenv[res.size()]);
+    }
+
+    public static String get(String key) {
+        String res = null;
+        for (Dotenv dotenv : dotenvFiles) {
+            res = dotenv.get(key.toUpperCase());
+            if (!res.isEmpty()) {
+                break;
+            }
+        }
+        return res;
+    }
+
+}
